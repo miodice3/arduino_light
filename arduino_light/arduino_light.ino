c8be1e0fde263f857143b932fcae9adcb969326c
michael <michael.iodice@smartersorting.com>
2022-01-22 13:04:49 -0700

    .env_variables.h type change, add wifi library and test, able to join guest networks

--- arduino_light/arduino_light.ino
+++ arduino_light/arduino_light.ino
@@ -1,11 +1,33 @@
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <WiFiScan.h>
+#include <ETH.h>
+#include <WiFiClient.h>
+#include <WiFiSTA.h>
+#include <WiFiServer.h>
+#include <WiFiType.h>
+#include <WiFiGeneric.h>
+
+
 #include "env_variables.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.print(network_name);
-  delay(500);
-  Serial.print(network_password);
-  
+    Serial.begin(9600);
+    Serial.println("Connecting to:");
+    Serial.println(ssid);
+    Serial.println(pass);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(1000);
+    }
+//    Serial.println(WiFi.localIP());
+    delay(1000);
 }
 
 void loop() {
