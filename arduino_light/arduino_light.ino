5bf85e14fc0f717eea8d4a9cca8a5e95eb86a7c4
michael <michael.iodice@smartersorting.com>
2022-01-22 12:14:20 -0700

    test .gitignore setup for env file

--- arduino_light/arduino_light.ino
+++ arduino_light/arduino_light.ino
@@ -1,9 +1,13 @@
+#include "env_variables.h"
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.print(network_name);
+  delay(500);
+  Serial.print(network_password);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
