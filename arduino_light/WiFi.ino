12c4663d54d6785814152c211589814fa17bd563
michael <michael.iodice@smartersorting.com>
2022-01-23 12:12:17 -0700

    feat: create and test reconnect method for network

--- arduino_light/WiFi.ino
+++ arduino_light/WiFi.ino
@@ -15,12 +15,37 @@
 void join_wifi_network(){
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pass);
-    Serial.print("Connecting to WiFi Network: " + String(ssid));
+    Serial.println("Connecting to WiFi Network: " + String(ssid));
     while (WiFi.status() != WL_CONNECTED) {
-      Serial.print('.');
+      Serial.println('.');
       delay(1000);
     }
     Serial.println(WiFi.localIP());
 }
 
-// TODO: test this block of code, possibly by updating guest network password with log statements.
+void re_join_network_if_needed(){
+    Serial.println("re_join_network_if_needed");
+    int c = 0;
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("within if block meaning connection to network failed");
+        int i = 0;
+    
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(ssid, pass);
+        Serial.println("Connecting to WiFi Network: " + String(ssid));
+        while (WiFi.status() != WL_CONNECTED) {
+            Serial.println("within while block");
+            Serial.println(i);
+            Serial.println('.');
+            i = i + 1;
+            delay(1000);
+                if (i >= 15) {
+                Serial.println("counter is: " + String(i));
+                Serial.println("resetting join network function");
+                re_join_network_if_needed();
+            }
+        }
+        Serial.println("Reconnected to WiFi Network: " + String(ssid));
+        Serial.println(WiFi.localIP());
+    }
+}
